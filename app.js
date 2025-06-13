009da902c242875250b8139e9d77689b310afe41
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-13 20:40:55 +0200

    add routes to main app

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const sanitize = require("./middlewares/sanitize");
+const userRoutes = require("./routes/userRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(sanitize);
+
+
+app.use("/users", userRoutes);
+app.use("/tasks", taskRoutes);
 
 
 sequelize.sync().then(() => {
