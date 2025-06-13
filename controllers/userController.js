6e9a11778aed455e0f9748c40e77a9382e339cc7
anggierz <anggie.rizo.zambrano@gmail.com>
2025-06-13 20:04:52 +0200

    add userRoutes and userController

---
+++ controllers/userController.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+exports.register = async (req, res) => {
+  try {
+    const {email, password} = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email: email,
+      password: hashedPassword
+    });
+    res.status(201).json("User registered successfully");
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Email and password are required' });
+    }
+
+    const user = await User.findOne({ where: { email: email } });
+    if (!user) return res.status(400).json({ message: 'User not found' });
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) return res.status(400).json({ message: 'Invalid password' });
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
